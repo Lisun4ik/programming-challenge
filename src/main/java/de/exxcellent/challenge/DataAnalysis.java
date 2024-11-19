d89efaafbf0138fe1746a0c5641b558a70f235b4
Lisun4ik <lisun4ik@yahoo.com>
2024-11-19 15:07:26 +0100

    Class for analyzing the data, with the possibility of determining the entry with a minimum difference.

---
+++ src/main/java/de/exxcellent/challenge/DataAnalysis.java
@@ -0,0 +1,43 @@
+package de.exxcellent.challenge;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataAnalysis {
+
+    /**
+     * Returns the entry with the minimum absolute difference from a list of entries.
+     *
+     * @param sourceData the source data
+     * @param captionOfId the caption of identifier
+     * @param captionOfVal1 the caption of the first value to compare
+     * @param captionOfVal2 the caption of the second value to compare
+     * @return the identifier with minimum difference
+     */
+    public static String getMinDiffEntryOfData(List<Map<String, String>> sourceData, String captionOfId, String captionOfVal1, String captionOfVal2) {
+        Map<String, Integer> diffList = new HashMap<>();
+        for (Map<String, String> data : sourceData) {
+            int val1 = 0;
+            int val2 = 0;
+            String id = "";
+            for (Map.Entry<String, String> entry : data.entrySet()) {
+                // the identifier
+                if (captionOfId.equals(entry.getKey())) id = entry.getValue();
+                // first value to compare
+                if (captionOfVal1.equals(entry.getKey())) val1 = Integer.parseInt(entry.getValue());
+                // second value to compare
+                if (captionOfVal2.equals(entry.getKey())) val2 = Integer.parseInt(entry.getValue());
+            }
+            // absolute difference
+            int diff = Math.abs(val1 - val2);
+            diffList.put(id, diff);
+        }
+
+        return diffList.entrySet().stream()
+                .min(Map.Entry.comparingByValue()) // which values
+                .map(Map.Entry::getKey)           // get the key
+                .orElse(null);
+
+    }
+}
