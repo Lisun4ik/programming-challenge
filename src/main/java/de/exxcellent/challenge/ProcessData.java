91233a9945ae716828506fcfe14ca6ea1e2984f0
Lisun4ik <lisun4ik@yahoo.com>
2024-12-03 15:27:25 +0100

    addition with a return value for the unit tests and the unit tests

--- src/main/java/de/exxcellent/challenge/ProcessData.java
+++ src/main/java/de/exxcellent/challenge/ProcessData.java
@@ -22,24 +22,26 @@ public class ProcessData {
      * processes data analysis for a CSV, or a JSON file
      *
      * @param source name of file to analyse
-     * @param type type of file
-     * @param id id of dataset
-     * @param val1 first value to compare (the order is irrelevant)
-     * @param val2 second value to compare (the order is irrelevant)
+     * @param type   type of file
+     * @param id     id of dataset
+     * @param val1   first value to compare (the order is irrelevant)
+     * @param val2   second value to compare (the order is irrelevant)
      */
-    public static void processDataOfFile(String source, SourceType type, String id, String val1, String val2) {
+    public static String processDataOfFile(String source, SourceType type, String id, String val1, String val2) {
         logger.info("processWeatherDataCsv - START");
         if (SourceType.WEB.equals(type)) {
             logger.info("wrong source type");
-            return;
+            return null;
         }
         try {
             List<Map<String, String>> data = (List<Map<String, String>>) SourceReaderUtils.readData(SourceStringUtils.getSourceString(source, type), type);
-            process(data, id, val1, val2);
+            String minVal = process(data, id, val1, val2);
+            logger.info("processWeatherDataCsv - END");
+            return minVal;
         } catch (Exception e) {
             logger.error("an error accured by reading the data from file " + Constants.weatherIdCaption, e.getMessage());
+            return null;
         }
-        logger.info("processWeatherDataCsv - END");
     }
 
     /**
@@ -49,16 +51,19 @@ public class ProcessData {
      *
      * @param url web server url
      */
-    public static void processWeatherDataOfWebSource(String url) {
+    public static String processWeatherDataOfWebSource(String url) {
+        logger.info("processWeatherDataOfWebSource - START");
         try {
             List<Map<String, String>> weatherData = (List<Map<String, String>>) SourceReaderUtils.readData(url, SourceType.WEB);
-            process(weatherData, Constants.weatherIdCaption, Constants.weatherVal1Caption, Constants.weatherVal2Caption);
+            String minVal = process(weatherData, Constants.weatherIdCaption, Constants.weatherVal1Caption, Constants.weatherVal2Caption);
+            logger.info("processWeatherDataOfWebSource - END");
+            return minVal;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void processFootballDataWeb() {
+    public static void processFootballDataWebSource() {
         // todo impl: it is necessary to create a web service first
         throw new UnsupportedOperationException("this method has not been implemented yet.");
     }
@@ -67,17 +72,19 @@ public class ProcessData {
      * processes the data analysis
      *
      * @param data data as a list of maps
-     * @param id id of dataset
+     * @param id   id of dataset
      * @param val1 first value to compare (the order is irrelevant)
      * @param val2 second value to compare (the order is irrelevant)
      */
-    private static void process(List<Map<String, String>> data, String id, String val1, String val2) {
+    private static String process(List<Map<String, String>> data, String id, String val1, String val2) {
         if (data != null && !data.isEmpty()) {
             IDataAnalysis<List<Map<String, String>>> dataAnalysis = new DataAnalysis();
             String dayWithSmallestTempSpread = dataAnalysis.getMinDiffEntryOfData(data, id, val1, val2);
             System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+            return dayWithSmallestTempSpread;
         } else {
             logger.info("no data found in source: " + Constants.weatherFileName);
+            return null;
         }
     }
 }
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,12 +1,26 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.analysis.DataAnalysis;
+import de.exxcellent.challenge.analysis.IDataAnalysis;
+import de.exxcellent.challenge.helper.DataConverter;
+import de.exxcellent.challenge.helper.SourceStringUtils;
+import de.exxcellent.challenge.source.Constants;
+import de.exxcellent.challenge.source.SourceReaderUtils;
+import de.exxcellent.challenge.source.SourceType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Example JUnit 5 test case.
+ *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 class AppTest {
@@ -23,9 +37,126 @@ class AppTest {
         assertEquals("successful", successLabel, "My expectations were not met");
     }
 
+
     @Test
-    void runFootball() {
-        App.main("--football", "football.csv");
+    void runSourceString() {
+        String actualPath = Constants.filePath + Constants.weatherFileName + "." + SourceType.CSV;
+        String pathToCompare = SourceStringUtils.getSourceString(Constants.weatherFileName, SourceType.CSV);
+        assertEquals(actualPath, pathToCompare);
+    }
+
+    @Test
+    void runMinDiffEntryOfData() {
+        List<Map<String, String>> testList = new ArrayList<>();
+
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("Title", "First id");
+        map1.put("Value1", "50");
+        map1.put("Value2", "120");
+
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("Title", "Second id");
+        map2.put("Value1", "110");
+        map2.put("Value2", "20");
+
+        Map<String, String> map3 = new HashMap<>();
+        map3.put("Title", "Third id");
+        map3.put("Value1", "5");
+        map3.put("Value2", "28");
+
+        testList.add(map1);
+        testList.add(map2);
+        testList.add(map3);
+
+        IDataAnalysis<List<Map<String, String>>> dataAnalysis = new DataAnalysis();
+        assertEquals(dataAnalysis.getMinDiffEntryOfData(testList, "Title", "Value1", "Value2"), "Third id");
+    }
+
+    @Test
+    void runReadCsvFile() {
+        try {
+            List<Map<String, String>> data = (List<Map<String, String>>) SourceReaderUtils.readData(SourceStringUtils.getSourceString(Constants.weatherFileName, SourceType.CSV), SourceType.CSV);
+            assertTrue(data != null && !data.isEmpty());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void runDataToJsonFile() {
+        try {
+            List<Map<String, String>> data = (List<Map<String, String>>) SourceReaderUtils.readData(SourceStringUtils.getSourceString(Constants.weatherFileName, SourceType.CSV), SourceType.CSV);
+            DataConverter.writeDataToJsonFile(data, Constants.weatherFileName);
+            String path = SourceStringUtils.getSourceString(Constants.weatherFileName, SourceType.JSON);
+            File f = new File(path);
+            assertTrue(f.exists() && !f.isDirectory());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void runReadJsonFile() {
+        try {
+            List<Map<String, String>> data = (List<Map<String, String>>) SourceReaderUtils.readData(SourceStringUtils.getSourceString(Constants.weatherFileName, SourceType.JSON), SourceType.JSON);
+            assertTrue(data != null && !data.isEmpty());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * todo: the server must be started before executing this method!
+     * important information: the server must be running successfully before the test is executed.
+     */
+    @Test
+    void runReadFromWeb() {
+        // todo: comment out for testing
+        /*
+        try {
+            List<Map<String, String>> data = (List<Map<String, String>>) SourceReaderUtils.readData(Constants.weatherWebService, SourceType.WEB);
+            assertTrue(data != null && !data.isEmpty());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+         */
+    }
+
+    @Test
+    void runWeatherCSV() {
+        String minVal = ProcessData.processDataOfFile(Constants.weatherFileName, SourceType.CSV, Constants.weatherIdCaption, Constants.weatherVal1Caption, Constants.weatherVal2Caption);
+        assertEquals(minVal, "14");
+    }
+
+    @Test
+    void runWeatherJSON() {
+        String minVal = ProcessData.processDataOfFile(Constants.weatherFileName, SourceType.JSON, Constants.weatherIdCaption, Constants.weatherVal1Caption, Constants.weatherVal2Caption);
+        assertEquals(minVal, "14");
+    }
+
+    /**
+     * todo: the server must be started before executing this method!
+     * important information: the server must be running successfully before the test is executed.
+     */
+    @Test
+    void runWeatherWeb() {
+        // todo: comment out for testing
+        /*
+        String minVal = ProcessData.processWeatherDataOfWebSource(Constants.weatherWebService);
+        assertEquals(minVal, "14");
+         */
+    }
+
+    @Test
+    void runFootballCSV() {
+        String minVal = ProcessData.processDataOfFile(Constants.footballFileName, SourceType.CSV, Constants.footballIdCaption, Constants.footballVal1Caption, Constants.footballVal2Caption);
+        assertEquals(minVal, "Aston_Villa");
+    }
+
+    @Test
+    void runFootballJSON() {
+        String minVal = ProcessData.processDataOfFile(Constants.footballFileName, SourceType.JSON, Constants.footballIdCaption, Constants.footballVal1Caption, Constants.footballVal2Caption);
+        assertEquals(minVal, "Aston_Villa");
     }
 
 }
